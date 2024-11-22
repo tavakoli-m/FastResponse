6d981facba84037edff9f3b74ccc846f955b7862
unknown <heyreza8@gmail.com>
2024-11-22 15:59:57 +0330

    init base response variable

--- src/BaseResponse/BaseResponse.php
+++ src/BaseResponse/BaseResponse.php
@@ -3,5 +3,10 @@
 namespace FastResponse\FastResponse\BaseResponse;
 
 class BaseResponse{
-    
+    private string|null $message = null;
+    private mixed $data = null;
+    private int $status = 200;
+
+    private array $appends = [];
+
 }
