0fc12ab3306a7106989a6ec1076f5480bc5660f6
unknown <heyreza8@gmail.com>
2024-11-22 16:14:46 +0330

    coding register method in fast response service provider

--- src/Providers/FastResponseServiceProvider.php
+++ src/Providers/FastResponseServiceProvider.php
@@ -2,8 +2,16 @@
 
 namespace FastResponse\FastResponse\Providers;
 
+use FastResponse\FastResponse\BaseResponse\BaseResponse;
+use FastResponse\FastResponse\Builder\ResponseBuilder;
 use Illuminate\Support\ServiceProvider;
 
-class FastResponseServiceProvider extends ServiceProvider{
-    
+class FastResponseServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->app->bind('fastResponse', function () {
+            return new ResponseBuilder(new BaseResponse);
+        });
+    }
 }
