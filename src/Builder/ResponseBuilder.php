2801ce242797ea9d7ac847f3554fdbfc362db56d
unknown <heyreza8@gmail.com>
2024-11-22 16:09:49 +0330

    add send method to response builder class

--- src/Builder/ResponseBuilder.php
+++ src/Builder/ResponseBuilder.php
@@ -7,4 +7,33 @@ use FastResponse\FastResponse\BaseResponse\BaseResponse;
 class ResponseBuilder
 {
     public function __construct(private BaseResponse $baseResponse) {}
+
+    public function withMessage(string $message)
+    {
+        $this->baseResponse->setMessage($message);
+        return $this;
+    }
+
+    public function withData(mixed $data)
+    {
+        $this->baseResponse->setData($data);
+        return $this;
+    }
+
+    public function withAppends(array $appends)
+    {
+        $this->baseResponse->setAppends($appends);
+        return $this;
+    }
+
+    public function withStatus(int $status)
+    {
+        $this->baseResponse->setStatus($status);
+        return $this;
+    }
+
+    public function send()
+    {
+        return $this->baseResponse->response();
+    }
 }
